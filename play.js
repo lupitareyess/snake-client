a3f2c1e9b3a2d7442b164930ea6a441afd4051b8
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-15 17:42:49 -0500

    Move function to own file, export and require

--- play.js
+++ play.js
@@ -3,23 +3,9 @@
 
 // Connect to server
 const net = require("net");
+// require function from client.js
+const { connect } = require('./client');
 
-// Create function establish connection
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-
-  conn.setEncoding("utf8");
-
-  // Handle incoming data and logs to user
-  conn.on('data', (data) => {
-    console.log("Server says:", data)
-  });
-
-  return conn;
-};
 
 console.log("Connecting...");
 connect();
