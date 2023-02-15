0cbbcc20bf9aa5fc61a11f68e9ce0ce9a97b9633
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-15 18:12:22 -0500

    Succesful connection msg, add name intitials

--- client.js
+++ client.js
@@ -9,6 +9,12 @@ const connect = function() {
 
   conn.setEncoding("utf8");
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server.")
+    conn.write("Name: LR")
+  })
+
+
   // Handle incoming data and logs to user
   conn.on('data', (data) => {
     console.log("Server says:", data)
