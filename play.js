e9db7eb0680cc60ce65adae91e6f6a419c50c76f
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-15 20:04:17 -0500

    Move functions, export & require

--- play.js
+++ play.js
@@ -1,31 +1,17 @@
 // Main file that will launch the game client
 
-// Setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput)
-
-  return stdin;
-};
 
 // Connect to server
 const net = require("net");
+
 // require function from client.js
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 console.log("Connecting...");
 connect();
+
 setupInput()
 
 
